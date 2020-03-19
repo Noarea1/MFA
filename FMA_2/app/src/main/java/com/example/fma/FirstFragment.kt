9609ea26f1b3673cefc5ec45590f3415fd532c04
Noarea1 <62275923+Noarea1@users.noreply.github.com>
2020-03-19 15:51:40 +0900

    FMA_2.8.1
    2番目の画面の背景と文字を配置。
    Safe Argsを有効化

--- FMA_2/app/src/main/java/com/example/fma/FirstFragment.kt
+++ FMA_2/app/src/main/java/com/example/fma/FirstFragment.kt
@@ -15,30 +15,35 @@ import androidx.navigation.fragment.findNavController
  */
 class FirstFragment : Fragment() {
 
+    //FirstFragmentのviewを生成
     override fun onCreateView(
+        //LayoutInflaterとコンテナの作成
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        //fragmentのレイアウトをxmlから生成
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
-
+    //onCreateViewで生成したviewを初期化
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+            //randomボタンが押された際、navigateメソッドの引数に指定されたIDのビューを取得し、そこに移動する
         view.findViewById<Button>(R.id.random_button).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        //toastボタンが押された際、画面下部に短時間Hello Toast!というトーストを表示する
         view.findViewById<Button>(R.id.toast_button).setOnClickListener{
             val myToast = Toast.makeText(context,"Hello Toast!", Toast.LENGTH_SHORT)
             myToast.show()
         }
+        //countボタンが押された際、countMeメソッドを使用し、ビューの数字を増やす
         view.findViewById<Button>(R.id.count_button).setOnClickListener {
             countMe(view)
         }
     }
     private fun countMe(view: View) {
-        //text view取得
+        //text　view_firstからtext view取得
         val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
         //text viewの値取得
         val countString = showCountTextView.text.toString()
